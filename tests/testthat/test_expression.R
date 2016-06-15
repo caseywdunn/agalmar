620ffecbb303ac38519909b0cb89819dfc50426c
CAT_MUNRO <catriona_munro@brown.edu>
2016-06-15 14:54:53 -0400

    adjusted test scripts

--- tests/testthat/test_expression.R
+++ tests/testthat/test_expression.R
@@ -1,10 +1,16 @@
 e=lapply(janedoe$expression, Expression)
 
-deseq=e[[1]]@DESeq2
+test_that("number of genes in DEseq2 object equals 397", {
+	expect_equal(length(rownames(e[[1]]@DESeq2)), 397)
+})
 
-deseq
+test_that("number of columns in object equals 14", {
+	expect_equal(length(colnames(e[[1]]@DESeq2)), 14)
+})
 
 #edgeR
-nrow(e[[1]]@edgeR$counts)
+test_that("number of genes in edgeR input equal 397", {
+	expect_equal(nrow(e[[1]]@edgeR$counts), 397)
+})
 
 
