ea70f71b8cbb60b9a391e911f3ce3a6319b9217e
CAT_MUNRO <catriona_munro@brown.edu>
2016-06-15 15:35:47 -0400

    testing effects of change

--- tests/testthat/test_expression.R
+++ tests/testthat/test_expression.R
@@ -1,7 +1,7 @@
 e=lapply(janedoe$expression, Expression)
 
 test_that("number of genes in DEseq2 object equals 397", {
-	expect_equal(length(rownames(e[[1]]@DESeq2)), 397)
+	expect_equal(length(rownames(e[[1]]@DESeq2)), 398)
 })
 
 test_that("number of columns in object equals 14", {
@@ -10,7 +10,7 @@ test_that("number of columns in object equals 14", {
 
 #edgeR
 test_that("number of genes in edgeR input equal 397", {
-	expect_equal(nrow(e[[1]]@edgeR$counts), 397)
+	expect_equal(nrow(e[[1]]@edgeR$counts), 398)
 })
 
 
